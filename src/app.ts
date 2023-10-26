d716ca0154babc9a5225517fd842e6459d0cf435
EnzoWM02 <enzowmosman@gmail.com>
2023-10-25 21:57:30 -0300

    - CORS fix

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,18 @@
 import express from "express";
-import httpSecurity from "middlewares/HttpSecurity";
 import routes from "routes/Routes";
+var cors = require("cors");
+import { env } from "config/globals";
 
 const app = express();
-const PORT = 3000;
 
+const corsOptions = {
+  origin: env.DOMAIN,
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
-app.use(httpSecurity);
 
 app.use("/api", routes);
 
-app.listen(PORT);
+app.listen(env.PORT);
