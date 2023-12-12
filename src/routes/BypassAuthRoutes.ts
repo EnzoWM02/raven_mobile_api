c6177d083b2f3ec0d888796e864542c4dfe7d8fa
EnzoWM02 <enzowmosman@gmail.com>
2023-12-11 21:34:08 -0300

    - Added Hello route to public apis

--- src/routes/BypassAuthRoutes.ts
+++ src/routes/BypassAuthRoutes.ts
@@ -1,10 +1,12 @@
 import registerAccountController from 'controller/login/RegisterAccountController';
 import loginControllerRoute from 'controller/login/LoginController';
 import { Router } from 'express';
+import helloWorldController from 'controller/helloWorld/HelloWorldController';
 
 const bypassAuthRoutes = Router();
 
 bypassAuthRoutes.use('/login', loginControllerRoute);
 bypassAuthRoutes.use('/register', registerAccountController);
+bypassAuthRoutes.use('/hello', helloWorldController);
 
 export default bypassAuthRoutes;
